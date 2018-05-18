3c637c2c7b53fd2b3c9a54f7bab50d022dfa69b1
Mcadima <moises.cadima@gmail.com>
2018-05-18 17:08:24 -0400

    Creación de una aplicación de consola C#. Donde se crea una clase, y en base a esa clase se manejan dos objetos. Se hace un ejemplo práctico y sencillo del manejo de sus atributos, constructor y destructor de la clase.

---
+++ Clases_C_Sharp/Models/Persona.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_C_Sharp.Models
+{
+
+    public class Persona
+    {
+        /*Atajo: Para crear una propiedad de manera más rápida, se debe escribir "prop"+TAB
+        Para crear getter y setter, al igual que en Java, el atajo es "propfull"+TAB.
+        Con propfull, se crean atributos o variables con encapsulamiento inmediato.
+        Ejemplo en SegundoNombre
+        */
+
+        public string PrimerNombre { get; set; }
+
+        #region Atributo SegundoNombre creado con propfull
+        private string segundoNombre;
+        public string SegundoNombre
+        {
+            get { return segundoNombre; }
+            set { segundoNombre = value; }
+        }
+        #endregion
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public bool EsHombre { get; set; }
+        public byte Edad { get; set; }
+        private static int ContadorDeObjetos = 0;
+        public int Id{ get; set; }
+
+        #region Manejo de eventos en memoria
+        #region Constructor
+        public Persona()
+            {
+            //Al crear un objeto persona,la constante le asignara un valor ID:
+            ContadorDeObjetos++;
+            Id = ContadorDeObjetos;
+            }
+            #endregion
+
+            #region Destructor
+            ~Persona()
+            {     
+                Console.WriteLine($"Destruyendo objeto con ID: {this.Id}");
+                Console.WriteLine("Presione cualquier tecla para continuar");
+                Console.ReadKey();
+            }
+            #endregion
+        #endregion
+
+
+        #region Manejo de funciones
+        /// <summary>
+        /// Creamos una función que nos retornará el sexo de manera más "bonita"
+        /// </summary>
+        /// <returns></returns>
+        public string getSexo()
+        {
+            StringBuilder tipoSexo = new StringBuilder();
+            if (this.EsHombre)
+            {
+                tipoSexo.Append("Masculino");
+            }
+            else
+            {
+                tipoSexo.Append("Femenino");
+            }
+            return tipoSexo.ToString();
+        }
+        #endregion
+
+
+      
+
+
+    }
+}
--- Clases_C_Sharp/Program.cs
+++ Clases_C_Sharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Clases_C_Sharp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,61 @@ namespace Clases_C_Sharp
     {
         static void Main(string[] args)
         {
+            /*Para iniciar el manejo de clases(Objetos), primero crearemos una carpeta llamada "Models",
+             * donde almacenaremos nuestros modelos de clases.
+             */
+
+            /*
+             * 1.- A través de una clase, creamos un objeto:
+             * Incluímos la libreria: using Clases_C_Sharp.Models;
+             */
+            Persona personaEjemplo = new Persona() {
+                Edad = 24,
+                EsHombre = true,
+                PrimerNombre = "Moisés",
+                SegundoNombre = "Alexis",
+                PrimerApellido = "Cadima",
+                SegundoApellido = "Figueroa"
+            };
+
+            /*
+             * Hemos creado una persona con el nombre de personaEjemplo, esta persona tiene valores iniciales indicados anteriormente.
+             * Mostraremos informacion a través de la consola:
+            */
+            //Creamos un string a través de StringBuilder, para hacer buen uso de la memoria:
+            StringBuilder MensajeSalida = new StringBuilder();
+            MensajeSalida.AppendLine($"El Id de la persona es: {personaEjemplo.Id}");
+            MensajeSalida.AppendLine($"Primer nombre: {personaEjemplo.PrimerNombre}");
+            MensajeSalida.AppendLine($"Segundo nombre: {personaEjemplo.SegundoNombre}");
+            MensajeSalida.AppendLine($"Primer apellido: {personaEjemplo.PrimerApellido}");
+            MensajeSalida.AppendLine($"Segundo apellido: {personaEjemplo.SegundoApellido}");
+            MensajeSalida.AppendLine($"La persona es de sexo: {personaEjemplo.getSexo()}");
+
+            Console.WriteLine($"{MensajeSalida.ToString()}");
+            Console.WriteLine("Presione cualquier tecla para continuar*****");
+            Console.ReadLine();
+
+
+            Persona personaEjemplo2 = new Persona()
+            {
+                Edad = 32,
+                EsHombre = true,
+                PrimerNombre = "Fabian",
+                SegundoNombre = "Alfonso",
+                PrimerApellido = "Norambuena",
+                SegundoApellido = "Perez"
+            };
+
+            MensajeSalida.Clear();
+            MensajeSalida.AppendLine($"El Id de la persona es: {personaEjemplo2.Id}");
+            MensajeSalida.AppendLine($"Primer nombre: {personaEjemplo2.PrimerNombre}");
+            MensajeSalida.AppendLine($"Segundo nombre: {personaEjemplo2.SegundoNombre}");
+            MensajeSalida.AppendLine($"Primer apellido: {personaEjemplo2.PrimerApellido}");
+            MensajeSalida.AppendLine($"Segundo apellido: {personaEjemplo2.SegundoApellido}");
+            MensajeSalida.AppendLine($"La persona es de sexo: {personaEjemplo2.getSexo()}");
+            Console.WriteLine($"{MensajeSalida.ToString()}");
+            Console.WriteLine("Presione cualquier tecla para continuar*****");
+            Console.ReadLine();
         }
     }
 }
